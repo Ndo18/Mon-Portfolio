4c6ad688f5ae066eb1462dc9a62859883e69a9e2
Ndo18 <samuel.dmb@outlook.fr>
2024-03-21 18:45:20 +0100

    📝 CV Updated

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -24,9 +24,9 @@ function Navbar () {
                     <li>
                         <a href="#Realisations" onClick={closeMenu}>Réalisations</a>
                     </li>
-                    <li>
+                    {/* <li>
                         <a href="#Activites" onClick={closeMenu}>Activités</a>
-                    </li>
+                    </li> */}
                     <li>
                         <a href="#Competences" onClick={closeMenu}>Compétences</a>
                     </li>
@@ -34,7 +34,7 @@ function Navbar () {
                         <a href="#Contact" onClick={closeMenu}>Contact</a>
                     </li>
                     <li>
-                        <a href="Samuel_DMB_CV_2023.pdf" target="_blank" rel="noopener noreferrer" onClick={closeMenu}>CV</a>
+                        <a href="Samuel_DOUMBE_LOBE_CV_Canada.pdf" target="_blank" rel="noopener noreferrer" onClick={closeMenu}>CV</a>
                     </li>
                 </ul>
             </nav>
